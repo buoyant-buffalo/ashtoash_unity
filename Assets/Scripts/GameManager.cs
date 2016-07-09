66936e595e8f1eef726e07b23876afda965dec6c
Brandon Williams <bwei.williams@gmail.com>
2016-07-08 19:47:05 -0600

    GameManager singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	private static GameManager _instance = null;
+
+	public static GameManager Instance 
+	{ 
+		get { return _instance; } 
+	}
+
+	void Awake () 
+	{
+		if (_instance != null && _instance != this)	
+		{
+			Destroy(gameObject);
+		}
+		else 
+		{
+			_instance = this;
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	void Update () 
+	{
+	
+	}
+}

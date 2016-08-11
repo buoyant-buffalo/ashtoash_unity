e11760b4711321fca340c15c2151760b2799f011
Brandon Williams <bwei.williams@gmail.com>
2016-08-10 22:29:11 -0600

    bridge collapses when walked on

---
+++ Assets/BridgeFall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeFall : MonoBehaviour {
+
+    public GameObject[] bridgeArray;
+	
+	void OnTriggerEnter () 
+    {
+        for (int i=0; i < bridgeArray.Length; i++) {
+            bridgeArray[i].GetComponent<Rigidbody> ().useGravity = true;
+            bridgeArray[i].GetComponent<MeshCollider> ().enabled = true;
+        }
+
+        Destroy(gameObject);
+	}
+}

00cefba531cf443148a67a7ba7f126dd8256d605
Brandon Williams <bwei.williams@gmail.com>
2016-08-25 00:56:54 -0600

    burn and interact scripts added/updated

---
+++ Assets/Scripts/Burn.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Burn : MonoBehaviour {
+
+    public float minDuration;
+
+    private Material burnMaterial;
+    private Color burnColor;
+
+    void Start()
+    {
+        burnMaterial = GetComponent<Renderer>().material;
+        StartCoroutine(burnTexture());
+    }
+
+    void Update()
+    {
+        burnMaterial.SetColor("_Color", burnColor);
+    }
+
+    public IEnumerator burnTexture()
+    {
+        float increment = 0, t = 0;
+        while(t < 1) //While t is less than 1 (meaning the affected is not toast yet), lerp the color towards burned toast!
+        {
+            burnColor = Color.Lerp(Color.white, Color.black, t);
+            increment = Time.deltaTime / minDuration;
+            t += Time.deltaTime / minDuration;
+            yield return new WaitForSeconds(increment);
+        }
+    }
+}

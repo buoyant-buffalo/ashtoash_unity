386b89a28f52d7e6d58aa131704377f0082f9562
jackykc <carrotnolie@gmail.com>
2016-08-27 21:44:37 -0600

    In folder 'Editor' added NodeEditor

---
+++ Assets/Editor/NodeBased/FullTreeNode.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class FullTreeNode : BaseInputNode {
+
+    //private string inputValue = "";
+
+
+    public FullTreeNode()
+    {
+        windowTitle = "FullTree";
+        hasInputs = false;
+
+    }
+    public override void DrawWindow()
+    {
+        base.DrawWindow();
+
+        //inputValue = EditorGUILayout.TextField("Value", inputValue);
+        if (GUILayout.Button("Create Tree"))
+        {
+            GameObject go = new GameObject(windowTitle);
+            //go.transform.position = new Vector3(0, 0, 0);
+
+            //calculateRandom();
+        }
+    }
+
+}
